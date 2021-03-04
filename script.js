81a00b9a837a323bbe1c086dc2dcf4e050012bca
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2021-03-04 09:50:51 +0700

    modify to fetch

--- script.js
+++ script.js
@@ -1,46 +1,98 @@
-$('.btn-search').on('click', function(){
-    $.ajax({
-        url: 'http://www.omdbapi.com/?apikey=e2b77af0&s='+ $('.input-movie').val(),
-        success: result => {
-            const movies = result.Search;
+// Using Ajax and jQuery
+// $('.btn-search').on('click', function(){
+//     $.ajax({
+//         url: 'http://www.omdbapi.com/?apikey=e2b77af0&s='+ $('.input-movie').val(),
+//         success: result => {
+//             const movies = result.Search;
+//             let cards = '';
+
+//             movies.forEach(m => {
+//                 cards += showMovie(m);
+//             });
+//             // console.log(movies);
+//             $('.movie-container').html(cards);
+
+//             // Ketika tombol di klik
+
+//             $('.modal-detail-button').on('click', function () {
+//                 $.ajax({
+//                     url: 'http://www.omdbapi.com/?apikey=e2b77af0&i='+ $(this).data('imdbid'),
+//                     success: m => {
+//                         // console.log($(this).data('imdbid'));
+//                         // console.log(m);
+//                         const movieDetail = detailMovie(m);
+
+//                         $('.modal-body').html(movieDetail);
+
+//                     },
+//                     error: (e) => {
+//                         console.log(e.responText);
+//                     }
+
+//                 });
+//             });
+
+//         },
+//         error: (e) => {
+//             console.log(e.responText);
+//         }
+//     });
+// });
+
+
+/*
+ Using Fetch
+ event input keyword movie and click button
+*/
+const searchButton = document.querySelector('.btn-search');
+searchButton.addEventListener('click', function () {
+
+    const inputKeyword = document.querySelector('.input-movie');
+    // console.log(inputKeyword.value);
+
+    //Call API
+    fetch('http://www.omdbapi.com/?apikey=e2b77af0&s=' + inputKeyword.value)
+        .then(response => response.json())
+        .then(response => {
+            const movies = response.Search;
             let cards = '';
-    
-            movies.forEach(m => {
-                cards += showMovie(m);
-            });
-            // console.log(movies);
-            $('.movie-container').html(cards);
-    
-            // Ketika tombol di klik
-    
-            $('.modal-detail-button').on('click', function () {
-                $.ajax({
-                    url: 'http://www.omdbapi.com/?apikey=e2b77af0&i='+ $(this).data('imdbid'),
-                    success: m => {
-                        // console.log($(this).data('imdbid'));
-                        // console.log(m);
+            movies.forEach(m => cards += showMovie(m));
+            const movieContainer = document.querySelector('.movie-container');
+            movieContainer.innerHTML = cards;
+
+            /*
+            Using Fetch 
+            event click button show detail movie
+            */
+            const buttonShowDetail = document.querySelectorAll('.modal-detail-button');
+
+            buttonShowDetail.forEach(btn => {
+                btn.addEventListener('click', function () {
+                    // console.log(this);
+                    const dataImdbid = this.dataset.imdbid;
+                    // console.log(dataImdbid);
+
+                    // Call API
+                    fetch('http://www.omdbapi.com/?apikey=e2b77af0&i='+ dataImdbid)
+                    .then(response => response.json())
+                    .then(m => {
                         const movieDetail = detailMovie(m);
-    
-                        $('.modal-body').html(movieDetail);
-    
-                    },
-                    error: (e) => {
-                        console.log(e.responText);
-                    }
-    
+                        const modalBody = document.querySelector('.modal-body');
+                        modalBody.innerHTML=movieDetail;
+                    })
+
+
                 });
             });
-    
-        },
-        error: (e) => {
-            console.log(e.responText);
-        }
-    });
+        })
 });
 
 
 
-function showMovie(m){
+
+
+
+function showMovie(m) {
     return ` <div class="col-md-4 my-3">
     <div class="card" >
         <img class="card-img-top" src="${m.Poster}" alt="Image">
@@ -53,7 +105,7 @@ function showMovie(m){
 </div>`;
 }
 
-function detailMovie(m){
+function detailMovie(m) {
     return ` <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
